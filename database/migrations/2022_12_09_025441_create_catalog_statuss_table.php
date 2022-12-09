a001d40595deaf80e9819000f5ffc63c13154178
Rakha Ridwan Virgiandi <rakhardwan@gmail.com>
2022-12-09 10:01:43 +0700

    Build catalog status migration and model

---
+++ database/migrations/2022_12_09_025441_create_catalog_statuss_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catalog_statuss', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('catalog_id')->references('id')->on('catalogs')->onDelete('cascade');
+            $table->enum('status',['pending','rejected','approved'])->default('pending');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catalog_statuss');
+    }
+};
